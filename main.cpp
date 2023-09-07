440d039b00a9f600c9af1c583bc77fd47138963b
鏡　桂一郎 <k022G0120@g.neec.ac.jp>
2023-09-07 13:03:35 +0900

    もうちょっと変えた

--- main.cpp
+++ main.cpp
@@ -9,26 +9,26 @@ const char kWindowTitle[] = "LE2D_14_ツチヤ_トモノリ";
 int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
 
 	int Block_PosX[4] = { 640, 640, 640, 640 };
-	int Block_PosY[4] = { -200,-400,-600,-800 };
+	int Block_PosY[4] = { -100,-350,-600,-850 };
 	int Block_Radius[4] = { 50, 50,  50,  50 };
 	int Block_color[4] = { 0,  1,   2,   3 };
 
-	int Block_i_PosX[4] = { 640, 640, 640, 640 };
-	int Block_i_PosY[4] = { -200,-400,-600,-800 };
-	int Block_i_Radius[4] = { 50, 50,  50,  50 };
-	int Block_i_color[4] = { 0,  1,   2,   3 };
+	int Block_strage_PosX[4] = { 200, 1080, 200, 1080 };
+	int Block_strage_PosY[4] = { 200, 200, 520, 520 };
+	int Block_strage_Radius[4] = { 50, 50, 50, 50 };
+	int Block_strage_color[4] = { 0,  1,   2,   3 };
 
-	int Hit_Circle_PosX[2]   = {640,640};
-	int Hit_Circle_PosY[2]   = {200,500};
-	int Hit_Circle_Radius[2] = {70, 70};
+	int Hit_Circle_PosX[2] = { 640,640 };
+	int Hit_Circle_PosY[2] = { 200,520 };
+	int Hit_Circle_Radius[2] = { 70, 70 };
 
 	int Box_Circle_PosX[4] = {};
 	int Box_Circle_PosY[4] = {};
 	int Box_Circle_Radius[4] = {};
 
-	int Block_Speed = 4;
+	int Block_Speed = 2;
 
-	int Flame = 60;
+	int Frame = 0;
 
 	int GameScene = 0;
 
@@ -73,10 +73,20 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
 			break;
 		}
 
-		Flame--;
+		Frame++;
+		if (Frame >= 2000) {
+			Block_Speed = 3;
+		}
+		if (Frame >= 4000) {
+			Block_Speed = 4;
+		}
+		if (Frame >= 7000) {
+			Block_Speed = 5;
+		}
+
 		for (int i = 0; i < 4; i++)
 		{
-			if (Block_PosX[i] == 640) 
+			if (Block_PosX[i] == 640)
 			{
 				Block_PosY[i] += Block_Speed;
 
@@ -85,33 +95,12 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
 			{
 			}
 
-
-			if (Block_PosY[i] >= 800)
+			if (Block_PosY[i] >= 850)
 			{
-				if (Flame <= 0)
-				{
-					Block_PosY[i] = -150;
-					Block_color[i] = rand() % 4;
-					Flame = 60;
-				}
-			}
-
-			if (180 <= Block_PosY[i] && Block_PosY[i] <= 220) 
-			{
-				if (Block_color[i] == 1)
-				{
-					if (keys[DIK_DOWN] && preKeys[DIK_DOWN] == 0)
-					{
-						Block_PosX[i] = 1080;
-						Block_PosY[i] = 200;
-					}
-				}
+				Block_PosY[i] = -150;
+				Block_color[i] = rand() % 4;
 			}
 		}
-
-
-
-
 
 
 		///
@@ -151,17 +140,46 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
 				Novice::DrawEllipse(Block_PosX[i], Block_PosY[i], Block_Radius[i], Block_Radius[i], 0.0f, BLACK, kFillModeSolid);
 			}
 
-			if (180 <= Block_PosY[i] && Block_PosY[i] <= 220)
+			if (170 <= Block_PosY[i] && Block_PosY[i] <= 230)
 			{
 				if (Block_color[i] == 0)
 				{
 					if (keys[DIK_UP] && preKeys[DIK_UP] == 0)
 					{
-						Novice::DrawEllipse(200, 200, Block_Radius[i], Block_Radius[i], 0.0f, RED, kFillModeSolid);
+						Novice::DrawEllipse(Block_strage_PosX[0], Block_strage_PosY[0], Block_Radius[i], Block_Radius[i], 0.0f, RED, kFillModeSolid);
+					}
+				}
+
+				if (Block_color[i] == 1)
+				{
+					if (keys[DIK_DOWN] && preKeys[DIK_DOWN] == 0)
+					{
+						Novice::DrawEllipse(Block_strage_PosX[1], Block_strage_PosY[1], Block_Radius[i], Block_Radius[i], 0.0f, BLUE, kFillModeSolid);
+					}
+				}
+			}
+
+			if (490 <= Block_PosY[i] && Block_PosY[i] <= 550)
+			{
+				if (Block_color[i] == 2)
+				{
+					if (keys[DIK_LEFT] && preKeys[DIK_LEFT] == 0)
+					{
+						Novice::DrawEllipse(Block_strage_PosX[2], Block_strage_PosY[2], Block_Radius[i], Block_Radius[i], 0.0f, GREEN, kFillModeSolid);
+					}
+				}
+
+				if (Block_color[i] == 3)
+				{
+					if (keys[DIK_RIGHT] && preKeys[DIK_RIGHT] == 0)
+					{
+						Novice::DrawEllipse(Block_strage_PosX[3], Block_strage_PosY[3], Block_Radius[i], Block_Radius[i], 0.0f, BLACK, kFillModeSolid);
 					}
 				}
 			}
 		}
+
+		Novice::ScreenPrintf(0, 0, "%d", Frame);
 
 		///
 		/// ↑描画処理ここまで
