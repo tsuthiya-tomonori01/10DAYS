0fc4e2ae11fd89a4a28bce5790a9954b95b72da2
鏡　桂一郎 <k022G0120@g.neec.ac.jp>
2023-09-07 11:37:47 +0900

    ちょっと変えた

--- main.cpp
+++ main.cpp
@@ -1,20 +1,21 @@
 #include <Novice.h>
 #include <math.h>
 #include <string>
+#include "time.h"
 
 const char kWindowTitle[] = "LE2D_14_ツチヤ_トモノリ";
 
 // Windowsアプリでのエントリーポイント(main関数)
 int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
 
-	int Block_PosX[4] = { 640, 640, 640, 640};
-	int Block_PosY[4] = {-200,-400,-600,-800};
-	int Block_Radius[4] = {50, 50,  50,  50};
-	int Block_color [4] = { 0,  1,   2,   3};
+	int Block_PosX[4] = { 640, 640, 640, 640 };
+	int Block_PosY[4] = { -200,-400,-600,-800 };
+	int Block_Radius[4] = { 50, 50,  50,  50 };
+	int Block_color[4] = { 0,  1,   2,   3 };
 
-	int Hit_Circle_PosX[2]   = {640,640};
-	int Hit_Circle_PosY[2]   = {200,500};
-	int Hit_Circle_Radius[2] = {70, 70};
+	int Hit_Circle_PosX[2] = { 640,640 };
+	int Hit_Circle_PosY[2] = { 200,520 };
+	int Hit_Circle_Radius[2] = { 70, 70 };
 
 	int Box_Circle_PosX[4] = {};
 	int Box_Circle_PosY[4] = {};
@@ -26,12 +27,14 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
 
 	int GameScene = 0;
 
+	srand((unsigned int)time(NULL));
+
 	// ライブラリの初期化
 	Novice::Initialize(kWindowTitle, 1280, 720);
 
 	// キー入力結果を受け取る箱
-	char keys[256] = {0};
-	char preKeys[256] = {0};
+	char keys[256] = { 0 };
+	char preKeys[256] = { 0 };
 
 	// ウィンドウの×ボタンが押されるまでループ
 	while (Novice::ProcessMessage() == 0) {
@@ -59,23 +62,45 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
 		case 4:
 			break;
 		}
-		
+
 		Flame--;
 		for (int i = 0; i < 4; i++)
 		{
-			Block_PosY[i] += Block_Speed;
+			if (Block_PosX[i] == 640) 
+			{
+				Block_PosY[i] += Block_Speed;
 
-			if (Block_PosY[i] >= 670)
+			}
+			else
+			{
+			}
+
+
+			if (Block_PosY[i] >= 800)
 			{
 				if (Flame <= 0)
 				{
-					Block_PosY[i] = -350;
+					Block_PosY[i] = -150;
 					Block_color[i] = rand() % 4;
 					Flame = 60;
 				}
 			}
+
+			if (180 <= Block_PosY[i] && Block_PosY[i] <= 220) 
+			{
+				if (Block_color[i] == 1)
+				{
+					if (keys[DIK_DOWN] && preKeys[DIK_DOWN] == 0)
+					{
+						Block_PosX[i] = 1080;
+						Block_PosY[i] = 200;
+					}
+				}
+			}
 		}
-		
+
+
+
 
 
 
@@ -86,14 +111,14 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
 		///
 		/// ↓描画処理ここから
 		///
-		
+
 		for (int h = 0; h < 2; h++)
 		{
 			Novice::DrawEllipse(Hit_Circle_PosX[h], Hit_Circle_PosY[h], Hit_Circle_Radius[h], Hit_Circle_Radius[h], 0.0f, WHITE, kFillModeSolid);
 
 			Novice::DrawEllipse(Hit_Circle_PosX[h], Hit_Circle_PosY[h], Hit_Circle_Radius[h], Hit_Circle_Radius[h], 0.0f, WHITE, kFillModeSolid);
 		}
-		
+
 		for (int i = 0; i < 4; i++)
 		{
 			if (Block_color[i] == 0)
@@ -114,6 +139,17 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
 			if (Block_color[i] == 3)
 			{
 				Novice::DrawEllipse(Block_PosX[i], Block_PosY[i], Block_Radius[i], Block_Radius[i], 0.0f, BLACK, kFillModeSolid);
+			}
+
+			if (180 <= Block_PosY[i] && Block_PosY[i] <= 220)
+			{
+				if (Block_color[i] == 0)
+				{
+					if (keys[DIK_UP] && preKeys[DIK_UP] == 0)
+					{
+						Novice::DrawEllipse(200, 200, Block_Radius[i], Block_Radius[i], 0.0f, RED, kFillModeSolid);
+					}
+				}
 			}
 		}
 
